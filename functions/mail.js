14298ac3476c098341489460fda81d581cf4de8e
Yohann Boniface <edhyjox@gmail.com>
2021-10-04 03:16:23 +0200

    :art: Improved overall code formatting

--- functions/mail.js
+++ functions/mail.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const queryString = require('querystring');
+
 // Nodemailer
-async function main(name,email,message,number) {
-  let testAccount = await nodemailer.createTestAccount();
+async function main(name, email, message, number) {
+    let testAccount = await nodemailer.createTestAccount();
 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.USER, // generated ethereal user
-      pass: process.env.PASS, // generated ethereal password
-    },
-    tls: {
-      ciphers:'SSLv3'
-      // maxVersion: 'TLSv1.2' 
-      },
-  })
+    let transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.USER, // generated ethereal user
+            pass: process.env.PASS, // generated ethereal password
+        },
+        tls: {
+            ciphers: 'SSLv3'
+            // maxVersion: 'TLSv1.2'
+        },
+    })
 
-  let info = await transporter.sendMail({
-    from: "noreply@bitecope.com", // sender address
-    to: "support@bitecope.com", // list of receivers
-    subject: "Got new client request", // Subject line
-    html: `<div style="margin: 80px 0px; background-color: black; width: 600px;">
+    let info = await transporter.sendMail({
+        from: "noreply@bitecope.com", // sender address
+        to: "support@bitecope.com", // list of receivers
+        subject: "Got new client request", // Subject line
+        html: `<div style="margin: 80px 0px; background-color: black; width: 600px;">
             <span>
                 <img src="https://i.imgur.com/kXSHgUe.png" referrerpolicy="no-referrer" style="border-radius: 6px 6px; height: 40px; width: auto;vertical-align: text-bottom;margin-left: 20px;margin-top: 18px;">
                 <label style="padding-left:20px;color: #00B2FF;font-family: 'Righteous';font-style: normal;font-weight: normal;font-size: 32px;">Bitecope</label>
@@ -40,35 +41,35 @@ async function main(name,email,message,number) {
                 Your Message: ${message}
             </h4>
         </div>`,
-  });
+    });
     console.log("Message sent: %s", info.messageId);
 }
 
 // acknowledgement to client
-async function ack(name,email,message,number) {
-  let testAccount = await nodemailer.createTestAccount();
-  console.log(process.env.USER);
-  let transporter = nodemailer.createTransport({
-    host: "smtp.office365.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: process.env.USER, // generated ethereal user
-      pass: process.env.PASS, // generated ethereal password
-    },
-    tls: {
-      ciphers:'SSLv3'
-      // maxVersion: 'TLSv1.2'
-    },
-  })
+async function ack(name, email, message, number) {
+    let testAccount = await nodemailer.createTestAccount();
+    console.log(process.env.USER);
+    let transporter = nodemailer.createTransport({
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.USER, // generated ethereal user
+            pass: process.env.PASS, // generated ethereal password
+        },
+        tls: {
+            ciphers: 'SSLv3'
+            // maxVersion: 'TLSv1.2'
+        },
+    })
 
-  // send mail with defined transport object
-  // console.log("email is"+email)
-  let info = await transporter.sendMail({
-    from: "noreply@bitecope.com", // sender address
-    to: email, // list of receivers
-    subject: "Welcome to Bitecope", // Subject line
-    html: `<div style="margin: 80px 0px; background-color: black; width: 600px;">
+    // send mail with defined transport object
+    // console.log("email is"+email)
+    let info = await transporter.sendMail({
+        from: "noreply@bitecope.com", // sender address
+        to: email, // list of receivers
+        subject: "Welcome to Bitecope", // Subject line
+        html: `<div style="margin: 80px 0px; background-color: black; width: 600px;">
             <span>
                 <img src="https://i.imgur.com/kXSHgUe.png" referrerpolicy="no-referrer" style="border-radius: 6px 6px; height: 40px; width: auto;vertical-align: text-bottom;margin-left: 20px;margin-top: 18px;">
                 <label style="padding-left:20px;color: #00B2FF;font-family: 'Righteous';font-style: normal;font-weight: normal;font-size: 32px;">Bitecope</label>
@@ -89,19 +90,20 @@ async function ack(name,email,message,number) {
             <h5 style="text-align: center;font-family: 'Montserrat',sans-serif;">For any further help write us at</h5>
             <h5 style="padding-bottom:30px;text-align: center;color: #00B2FF;font-family: 'Montserrat',sans-serif;"><a href="mailto:support@bitecope.com" style="text-decoration: none;">support@bitecope.com</a></h5>
         </div>`,
-  });
-  console.log("Message sent: %s", info.messageId);
+    });
+    console.log("Message sent: %s", info.messageId);
 }
+
 // main().catch(console.error);
 // ack().catch(console.error);
-exports.handler = async function(event){
-  console.log(event.body);
-  const {name,email,message,number} = JSON.parse(event.body);
-  console.log(name,email,message,number);
-  main(name,email,message,number).catch(console.error);
-  ack(name,email,message,number).catch(console.error);
-  
-  return {statusCode:200}
- 
+exports.handler = async function (event) {
+    console.log(event.body);
+    const {name, email, message, number} = JSON.parse(event.body);
+    console.log(name, email, message, number);
+    main(name, email, message, number).catch(console.error);
+    ack(name, email, message, number).catch(console.error);
+
+    return {statusCode: 200}
+
 };
 
--- public/FAQ.js
+++ public/FAQ.js
@@ -1,27 +1,27 @@
 let pricingInfo = [
     {
-     "name": "What are the charges for Bitecope app?",
-     "content": "Bitecope is based on monthly subscription model (for more details contact support@bitecope.com), however user are provided with 30 day free trail for the first instance when they Sign up. So that, user can try & test the app features and assess its use-case before buying it. "
+        "name": "What are the charges for Bitecope app?",
+        "content": "Bitecope is based on monthly subscription model (for more details contact support@bitecope.com), however user are provided with 30 day free trail for the first instance when they Sign up. So that, user can try & test the app features and assess its use-case before buying it. "
     },
     {
         "name": "Do we provide refunds?",
-        "content": "Yes we do provide refunds, only if user have bought the annual subscription. "+"\n"+"Note : Refund is only available for whole package. It's not valid for individual accounts. "
+        "content": "Yes we do provide refunds, only if user have bought the annual subscription. " + "\n" + "Note : Refund is only available for whole package. It's not valid for individual accounts. "
     },
     {
         "name": "How the refund amount is calculated?",
-        "content":"So, for example if user has bought our subscription then they will have to pay 7500 INR in total. That means they are paying 625 INR per month. If user decides to cancel the subscription then, we calculate the remaining month and multiple that with 625 "+"\n\n"+" For example : "+"\n"+"- You bought subscription on 20th November 2021. "+"\n"+" - Subscription is Vaild till 20th November 2022. "+"\n"+" Now, if you cancel subscription on 3rd March then you get money reimbursed of 7 months which is 7 x 625 = 4375 INR *excluding charges.*"
+        "content": "So, for example if user has bought our subscription then they will have to pay 7500 INR in total. That means they are paying 625 INR per month. If user decides to cancel the subscription then, we calculate the remaining month and multiple that with 625 " + "\n\n" + " For example : " + "\n" + "- You bought subscription on 20th November 2021. " + "\n" + " - Subscription is Vaild till 20th November 2022. " + "\n" + " Now, if you cancel subscription on 3rd March then you get money reimbursed of 7 months which is 7 x 625 = 4375 INR *excluding charges.*"
     },
     {
         "name": "What are the modes of payment for purchasing Bitecope?",
-        "content":"We accept online payments through credit/debit card, netbanking and UPI. "
+        "content": "We accept online payments through credit/debit card, netbanking and UPI. "
     },
     {
         "name": "How long will it take to process the refund?",
-        "content":"Within 30 days money will be returned."
+        "content": "Within 30 days money will be returned."
     }
- ]
+]
 
- let productInfo = [
+let productInfo = [
     {
         "name": "What kind of businesses can use this app?",
         "content": "Manufacturing companies belonging to any sectors can use this app. Size of the company also does not matter."
@@ -32,44 +32,44 @@ let pricingInfo = [
     },
     {
         "name": "How can I manage worker permission?",
-        "content":"Not yet! This feature will be implemented soon."
+        "content": "Not yet! This feature will be implemented soon."
     },
     {
         "name": "Can I access the same Bitecope account from two mobile devices?",
-        "content":"Yes, you can do it."
+        "content": "Yes, you can do it."
     },
     {
         "name": "Is Bitecope Cloud/Web based?",
-        "content":"Bitecope is still in process of launching this services, Once it's launched we will notify all our users. Your patience is highly appreciated."
+        "content": "Bitecope is still in process of launching this services, Once it's launched we will notify all our users. Your patience is highly appreciated."
     }
- ]
+]
 
 
-    let acc1 = document.querySelectorAll('.accordion1');
-    let acc2 = document.querySelectorAll('.accordion2');
-    let content1 = document.querySelectorAll('.Acc-Content1');
-    let content2 = document.querySelectorAll('.Acc-Content2');
-    let accDiv1 = document.querySelector('.accDiv-1');
-    let accDiv2 = document.querySelector('.accDiv-2');
-    let faq = document.querySelectorAll('.faq');
-    accordion(acc1,pricingInfo,content1,accDiv1);
-    accordion(acc2,productInfo,content2,accDiv2);
-    
-    function accordion(acc,info,content,accDiv){
+let acc1 = document.querySelectorAll('.accordion1');
+let acc2 = document.querySelectorAll('.accordion2');
+let content1 = document.querySelectorAll('.Acc-Content1');
+let content2 = document.querySelectorAll('.Acc-Content2');
+let accDiv1 = document.querySelector('.accDiv-1');
+let accDiv2 = document.querySelector('.accDiv-2');
+let faq = document.querySelectorAll('.faq');
+accordion(acc1, pricingInfo, content1, accDiv1);
+accordion(acc2, productInfo, content2, accDiv2);
 
-          for (let i = 0; i < acc.length; i++) {
-            acc[i].innerText = info[i].name;
-            content[i].innerText = info[i].content;
+function accordion(acc, info, content, accDiv) {
 
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                let panel = this.nextElementSibling;
-                if (panel.style.display === "block") {
+    for (let i = 0; i < acc.length; i++) {
+        acc[i].innerText = info[i].name;
+        content[i].innerText = info[i].content;
+
+        acc[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            let panel = this.nextElementSibling;
+            if (panel.style.display === "block") {
                 panel.style.display = "none";
-                } else {
+            } else {
                 panel.style.display = "block";
-                }
-            });
-        }
-
+            }
+        });
     }
+
+}
