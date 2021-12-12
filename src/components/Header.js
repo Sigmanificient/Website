335a5e63dbe161444a117f6c47206811617f5bc1
sigmanificient <sigmanificient@gmail.com>
2021-12-12 18:03:55 +0100

    :lipstick: Hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React, { useState } from "react"
 
 function Header() {
-  return (
+    const [navbarOpen, setNavbarOpen] = useState(false)
+    const handleToggle = () => {setNavbarOpen(!navbarOpen)}
+
+    return (
     <React.Fragment>
+        <script>hamburger()</script>
         <header className="header">
             <div className="header--container">
                 <a className="logo" href="#">
                     <img src="svg/logo/logo__no_bg.svg" alt="Bite Cope Startup logo"/>
                     Bitecope
                 </a>
-                <nav className="nav">
-                    <a className="nav--link" href="#">Home</a>
-                    <a className="nav--link" href="#about_us">About Us</a>
-                    <a className="nav--link" href="#contact-us">Contact Us</a>
+
+                <a className="nav--link" href="/">Home</a>
+                <a className="nav--link" href="/">Subscription</a>
+                <a className="nav--link" href="/#contact-us">Contact Us</a>
+
+                <nav id="menu" className={navbarOpen? 'nav open': 'nav'}>
+                    <img id="closer" src="svg/cross-button.svg" alt="Close" onClick={handleToggle}/>
+
+                    <a className="nav--link responsive-nav" href="/">Home</a>
+                    <a className="nav--link responsive-nav" href="/">Subscription</a>
+                    <a className="nav--link responsive-nav" href="/#contact-us">Contact Us</a>
+
+                    <a className="nav--link" href="/#about_us">About Us</a>
                     <a className="nav--link" href="/t&c">T&C</a>
                     <a className="nav--link" href="/faq">FAQ</a>
                     <a className="nav--link" href="/privacy">Privacy Policy</a>
                 </nav>
 
-                <a id="hamburger" href="#" className="hamburger">
+                <a id="hamburger" className={navbarOpen ? 'hamburger open' : 'hamburger'} onClick={handleToggle}>
                     <img src="svg/button_hamburger.svg" alt="+"/>
                 </a>
             </div>
